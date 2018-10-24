7ab117dd97b703a28ab4b4f7e45ead625a7144c1
Kun Huang <kkun.huang@gmail.com>
2018-10-23 21:26:11 -0400

    Initial Commit

---
+++ TinderTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderTest
+//
+//  Created by Kun Huang on 10/23/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
