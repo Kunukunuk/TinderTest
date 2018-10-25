5f3329e20e5729725ebe7e60a37f14252b277e44
Kun Huang <kkun.huang@gmail.com>
2018-10-24 22:18:57 -0400

    made card view controller

---
+++ TinderTest/CardsViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CardsViewController.swift
+//  TinderTest
+//
+//  Created by Kun Huang on 10/24/18.
+//  Copyright © 2018 Kun Huang. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    @IBOutlet weak var cardImageView: UIImageView!
+    var cardInitialCenter: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let image = UIImage(named: "nav_bar")
+        let imageView = UIImageView(image: image)
+        self.navigationItem.titleView = imageView
+        
+        cardInitialCenter = cardImageView.center
+        
+    }
+    
+    @IBAction func panImage(_ sender: UIPanGestureRecognizer) {
+        
+        //let location = sender.location(in: view)
+        //let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            
+            print("Gesture began")
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+        } else if sender.state == .ended {
+            print("Gesture ended")
+        }
+        
+    }
+    
+    
+    @IBAction func buttonTap(_ sender: UIButton) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
